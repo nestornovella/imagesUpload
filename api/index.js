63542844b90058ce0210ace40e9e27b48a2a925d
Nestor Novella <importarnorth@gmail.com>
2023-07-17 17:32:24 -0300

    achanges

--- api/index.js
+++ api/index.js
@@ -3,13 +3,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 const router = require('./src/routes/indexRoutes')
 const { connection } = require('./src/db')
-
+const port = process.env.PORT || 3001
 
 const server = express()
 server.use(morgan('dev'))
-server.use(cors())
+server.use(cors()) 
 server.use(express.json())
 server.use(router)
 
 
-server.listen(3001, connection.sync({force:true}).then(()=> console.log('listen in port 3001')))
+server.listen(3001, connection.sync({force:false}).then(()=> console.log('listen in port 3001')))
--- api/src/db.js
+++ api/src/db.js
@@ -4,10 +4,26 @@ const {Sequelize} =require('sequelize')
 const sequelize = new Sequelize(process.env.SEQUELIZE_URL, {
     logging:false,
     native:false,
-    dialect:'postgres'
+    dialect:'postgres',
+    dialectOptions: {
+              ssl: {
+                require: true,
+                rejectUnauthorized: false, // Solo si tienes problemas con los certificados SSL
+              },
+            }
 })
 
-
+// const sequelize = new Sequelize('pruebasDb', 'usuario', 'Ciro1990', {
+//     host: 'pruebasdb.cbe9cr0k1a4u.us-west-2.rds.amazonaws.com',
+//     port: 5432,
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false, // Solo si tienes problemas con los certificados SSL
+//       },
+//     },
+//   });
 const Product = require('./models/product')
 Product(sequelize)
 
